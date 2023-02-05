93fade4a3c8320c52ee447ac1c6365f015393e96
adelie-kimtaewook <109192067+adelie-kimtaewook@users.noreply.github.com>
2023-02-05 23:43:27 +0900

    기존 영업자 완료

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+	//header
 	$("#menuToggle").on("change", function () {
 		if ($(this).is(":checked")) {
 			$("html").addClass("scrollNone");
@@ -7,5 +8,16 @@ $(document).ready(function () {
 			$("html").removeClass("scrollNone");
 			$(".kcraedu").removeClass("kcraedu--menu");
 		}
+	});
+
+	$(".header__gnb-item a").on("click", function () {
+		$(".header__gnb-item").removeClass("active");
+		$(this).parents(".header__gnb-item").addClass("active");
+	});
+
+	//toggle
+	$(".btn-toggle").on("click", function () {
+		var target = $(this).attr("href");
+		$(target).toggleClass("d-none");
 	});
 });
